cfc272c0c2228537f65e77b75379a99951440be3
chakibamine <104866605+chakibamine@users.noreply.github.com>
2025-04-17 17:08:40 +0100

    create backend project

---
+++ Backend/src/main/java/com/backend/stageconnect/StageConnectApplication.java
@@ -0,0 +1,13 @@
+package com.backend.stageconnect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StageConnectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StageConnectApplication.class, args);
+    }
+
+}
---
+++ Backend/src/test/java/com/backend/stageconnect/StageConnectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.stageconnect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StageConnectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
